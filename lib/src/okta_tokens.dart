148e0d5a53e15c68b20ad7c9e730b54902b0c18c
0x0-14 <saigklkrishnan@gmail.com>
2022-11-30 14:12:18 +0530

    Typecasted Okta Model's fromMap's parameter to avoid uninitialized variables

--- lib/src/okta_tokens.dart
+++ lib/src/okta_tokens.dart
@@ -2,19 +2,24 @@ class OktaResponse {
   final String? id;
   final String? accessToken;
   final bool reEnroll;
+  final bool codeSentFor2FA;
 
-  OktaResponse({
-    required this.id,
-    required this.accessToken,
-    required this.reEnroll,
-  });
+  OktaResponse(
+      {required this.id,
+      required this.accessToken,
+      required this.reEnroll,
+      required this.codeSentFor2FA});
 
-  factory OktaResponse.parse(map) {
+  factory OktaResponse.parse(Map? map) {
+    if (map == null) {
+      throw Exception('Empty Okta Result');
+    }
     return OktaResponse(
       id: map['userId'] != null ? map['userId'] as String : null,
       accessToken:
           map['accessToken'] != null ? map['accessToken'] as String : null,
       reEnroll: map['reEnroll'] != null ? true : false,
+      codeSentFor2FA: map['codeSentFor2FA'] != null ? true : false,
     );
   }
 }
